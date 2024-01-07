f153deaff34456cd69a04effd314c08812b08399
StefanHJC <sapanowicz@gtapstudio.com>
2024-01-07 18:01:42 +0400

    Removed old update system

---
+++ Assets/Scripts/Infrastructure/BootstrapInstaller.cs
@@ -0,0 +1,20 @@
+using Zenject;
+
+namespace ShootEmUp
+{
+    public class BootstrapInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+
+        }
+    }
+
+    public class SceneInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+
+        }
+    }
+}
