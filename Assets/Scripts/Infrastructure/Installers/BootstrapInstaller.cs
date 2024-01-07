4a9372b4ae52dac89d5c0d2c10754efab09a8ed4
StefanHJC <sapanowicz@gtapstudio.com>
2024-01-07 21:10:01 +0400

    Zenject bindings

--- Assets/Scripts/Infrastructure/Installers/BootstrapInstaller.cs
+++ Assets/Scripts/Infrastructure/Installers/BootstrapInstaller.cs
@@ -6,26 +6,12 @@ namespace ShootEmUp
     {
         public override void InstallBindings()
         {
-            BindGameManager();
             BindAssetProvider();
         }
 
         private void BindAssetProvider()
         {
-            Container.
-                Bind<AssetProvider>().
-                FromNew().
-                AsSingle().
-                Lazy();
-        }
-
-        private void BindGameManager()
-        {
-            Container.
-                Bind<GameManager>().
-                FromNew().
-                AsSingle().
-                Lazy();
+            Container.Bind<AssetProvider>().FromNew().AsSingle().Lazy();
         }
     }
 }
--- Assets/Scripts/Infrastructure/Installers/SceneInstaller.cs
+++ Assets/Scripts/Infrastructure/Installers/SceneInstaller.cs
@@ -6,7 +6,92 @@ namespace ShootEmUp
     {
         public override void InstallBindings()
         {
+            BindInput();
+            BindGameEndListener();
+            BindGameManager();
 
+            BindEnemyFactory();
+            BindEnemyPool();
+            BindEnemyFactory();
+            BindEnemyManager();
+
+            BindBulletBuilder();
+            BindBulletPool();
+            BindBulletSystem();
+
+            BindHUD();
+        }
+
+        private void BindGameManager()
+        {
+            Container.
+                Bind<GameManager>().
+                FromNew().
+                AsSingle().
+                Lazy();
+        }
+
+        private void BindBulletSystem()
+        {
+            Container.
+                BindInterfacesAndSelfTo<BulletSystem>().
+                AsSingle();
+        }
+
+        private void BindInput()
+        {
+            Container.
+                BindInterfacesAndSelfTo<InputManager>().
+                AsSingle();
+        }
+
+        private void BindBulletPool()
+        {
+            Container.
+                Bind<BulletPool>().
+                AsSingle();
+        }
+
+        private void BindBulletBuilder()
+        {
+            Container.
+                Bind<BulletBuilder>().
+                AsSingle();
+        }
+
+        private void BindHUD()
+        {
+            Container.
+                Bind<HUD>().
+                AsSingle();
+        }
+
+        private void BindEnemyManager()
+        {
+            Container.
+                Bind<EnemyManager>().
+                AsSingle();
+        }
+
+        private void BindEnemyPool()
+        {
+            Container.
+                Bind<EnemyPool>().
+                AsSingle();
+        }
+
+        private void BindEnemyFactory()
+        {
+            Container.
+                Bind<EnemyFactory>().
+                AsSingle();
+        }
+
+        private void BindGameEndListener()
+        {
+            Container.
+                Bind<GameEndListener>().
+                AsSingle();
         }
     }
 }
