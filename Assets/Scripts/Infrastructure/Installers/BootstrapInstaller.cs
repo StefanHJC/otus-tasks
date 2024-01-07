81ba5db24d99eda988b9753078971b37c928fe89
StefanHJC <sapanowicz@gtapstudio.com>
2024-01-07 20:15:19 +0400

    Boot scene

--- Assets/Scripts/Infrastructure/Installers/BootstrapInstaller.cs
+++ Assets/Scripts/Infrastructure/Installers/BootstrapInstaller.cs
@@ -6,7 +6,26 @@ namespace ShootEmUp
     {
         public override void InstallBindings()
         {
+            BindGameManager();
+            BindAssetProvider();
+        }
 
+        private void BindAssetProvider()
+        {
+            Container.
+                Bind<AssetProvider>().
+                FromNew().
+                AsSingle().
+                Lazy();
+        }
+
+        private void BindGameManager()
+        {
+            Container.
+                Bind<GameManager>().
+                FromNew().
+                AsSingle().
+                Lazy();
         }
     }
 }
