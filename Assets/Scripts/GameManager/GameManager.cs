8877365507baf5958a3303aaeb51979433e37f84
HJC <flieger-luf@yandex.ru>
2024-01-23 22:59:45 +0300

    Minor rework

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -65,32 +65,34 @@ namespace ShootEmUp
 
     public class GameLauncher
     {
+        private readonly UIMediator _uiMeddiator;
+
         [Inject]
-        public GameLauncher()
+        public GameLauncher(UIMediator uiMediator)
         {
-
-        }
-
-        private async Task SetGameStartDelayAsync(int delayInSeconds)
-        {
-            int i = 0;
-            _hud.ScreenTextRenderer.Enable();
-
-            while (i <= delayInSeconds)
-            {
-                _hud.ScreenTextRenderer.Text = (delayInSeconds - i++).ToString();
-                await Task.Delay(millisecondsDelay: 1000);
-            }
-            _hud.ScreenTextRenderer.Disable();
+            _uiMeddiator = uiMediator;
         }
 
         public async void StartGameAsync()
         {
-            await SetGameStartDelayAsync(delayInSeconds: GameStartDelay);
+            await AwaitGameStartAsync(delayInSeconds: 3);
 
-            InstallGameSessionBindings(InstantiateCharacterView(at: _characterPosition, prefab: _characterView));
+            //InstallGameSessionBindings(InstantiateCharacterView(at: _characterPosition, prefab: _characterView));
             //_gameListenersController.StartGame();
-            _hud.PauseButton.Enable();
+            //_hud.PauseButton.Enable();
+        }
+
+        private async Task AwaitGameStartAsync(int delayInSeconds)
+        {
+            int i = 0;
+
+            while (i <= delayInSeconds)
+            {
+                _uiMeddiator.ShowScreenText((delayInSeconds - i++).ToString());
+               
+                await Task.Delay(millisecondsDelay: 1000);
+            }
+            _uiMeddiator.HideScreenText();
         }
     }
 
@@ -165,18 +167,17 @@ namespace ShootEmUp
             _pauseButton.Clicked += InvokeResumeButtonEvent;
         }
 
-        private void InvokeStartButtonEvent() => OnStartFired?.Invoke();
-
-        private void InvokeResumeButtonEvent() => OnResumeFired?.Invoke();
-
-        private void InvokePauseButtonEvent() => OnPauseFired?.Invoke();
-
-
         public void Dispose()
         {
             _startButton.Clicked -= InvokeStartButtonEvent;
             _resumeButton.Clicked -= InvokePauseButtonEvent;
             _pauseButton.Clicked -= InvokeResumeButtonEvent;
         }
+
+        private void InvokeStartButtonEvent() => OnStartFired?.Invoke();
+
+        private void InvokeResumeButtonEvent() => OnResumeFired?.Invoke();
+
+        private void InvokePauseButtonEvent() => OnPauseFired?.Invoke();
     }
 }
